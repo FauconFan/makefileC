8f50d1f015269718329a89c5d537da31be609806
Fauconfan <joseph0priou@gmail.com>
2021-12-11 12:56:14 +0100

    test first CI 55

--- tests/scripts/functional_tests.bash
+++ tests/scripts/functional_tests.bash
@@ -7,12 +7,20 @@ NAME_DEBUG="./$(grep NAME_DEBUG < config.mk | cut -d'=' -f2 | xargs)"
 
 ALL_RULES="all clean fclean re debug redebug"
 
-ls -lRs > /tmp/tree_hierarchy					# register project architecture for later
+ls -lRs > /tmp/tree_hierarchy # register project architecture for later
 
 ## Check all availables commands
 for rule in ${ALL_RULES}
 do
 	make "${rule}"
+done
+
+## Check that fclean full cleans on every command
+for rule in ${ALL_RULES}
+do
+	make "${rule}"
+	make fclean
+	diff <(ls -lRs) /tmp/tree_hierarchy
 done
 
 ## Check verbosity
@@ -31,42 +39,32 @@ done
 
 make fclean # resets
 
-
-make all > /tmp/build_all
-make fclean
-make all
-
 ## Check tree and output consistency on release build
 make all
-eval "${NAME}" > /tmp/release_output			# register release output for later
+eval "${NAME}" > /tmp/release_output # register release output for later
 make clean
-diff <(eval "${NAME}") /tmp/release_output		# check diff between old output and binary after cleaned
+diff <(eval "${NAME}") /tmp/release_output
 make fclean
-diff <(ls -lRs) /tmp/tree_hierarchy				# check diff between old project architecture and the actual project architecture
 make re
-diff <(eval "${NAME}") /tmp/release_output		# check diff between old output and binary after re
+diff <(eval "${NAME}") /tmp/release_output
 make fclean
-diff <(ls -lRs) /tmp/tree_hierarchy				# check diff between old project architecture and the actual project architecture
 
 ## Check tree and output consistency on debug build
 make debug
-eval "${NAME_DEBUG}" > /tmp/debug_output		# register debug output for later
+eval "${NAME_DEBUG}" > /tmp/debug_output # register debug output for later
 make clean
-diff <(eval "${NAME_DEBUG}") /tmp/debug_output	# check diff between old debug output and binary after cleaned
+diff <(eval "${NAME_DEBUG}") /tmp/debug_output
 make fclean
-diff <(ls -lRs) /tmp/tree_hierarchy				# check diff between old project architecture and the actual project architecture
 make redebug
-diff <(eval "${NAME_DEBUG}") /tmp/debug_output	# check diff between old debug output and binary after re
+diff <(eval "${NAME_DEBUG}") /tmp/debug_output
 make fclean
-diff <(ls -lRs) /tmp/tree_hierarchy				# check diff between old project architecture and the actual project architecture
 
 ## Check debug informations in binaries
 make all
-test "$(objdump --syms "${NAME}" | grep -c debug)" -eq 0		# check if there are no debug informations in release binary
+test "$(objdump --syms "${NAME}" | grep -c debug)" -eq 0
 make debug
-test "$(objdump --syms "${NAME_DEBUG}" | grep -c debug)" -gt 0	# check if there are debug informations in debug binary
+test "$(objdump --syms "${NAME_DEBUG}" | grep -c debug)" -gt 0
 make fclean
-diff <(ls -lRs) /tmp/tree_hierarchy								# check diff between old project architecture and the actual project architecture
 
 ## Remove useless files
 rm -f \
