beab3719a6c303212cc34247ebd42228d0c5b188
Fauconfan <joseph0priou@gmail.com>
2021-12-12 13:47:01 +0100

    test first CI 77

--- tests/error-handling/all.bash
+++ tests/error-handling/all.bash
@@ -7,14 +7,14 @@ if [ ! "$(git rev-parse --show-prefix)" = "tests/error-handling/mini-project2/" 
 	exit 1
 fi
 
-! make
+make && exit 1
 cp ../11-too-few-variables.config.mk config.mk
-! make
+make && exit 1
 cp ../12-too-much-variables.config.mk config.mk
-! make
+make && exit 1
 cp ../13-specified-files-missing.mk config.mk
-! make
+make && exit 1
 cp ../14-presence-unspecified-files.mk config.mk
-! make
+make && exit 1
 
 rm -f config.mk
