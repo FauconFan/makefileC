3a9f8e7aed1e211ffe30fd9eb82aabb7d9377ac8
Joseph Priou <joseph0priou@gmail.com>
2023-03-21 10:56:08 +0000

    fix objdump printing debug symbols 2

--- tests/compilation/_all_vs_debug.bash
+++ tests/compilation/_all_vs_debug.bash
@@ -22,10 +22,16 @@ make redebug
 diff <(eval "${NAME_DEBUG}") /tmp/debug_output
 make fclean
 
+# How to get debug symbols from objdump
+# https://stackoverflow.com/questions/3284112/how-to-check-if-program-was-compiled-with-debug-symbols
+# Unfortunately, it appears the objdump does not print debug symbols by default,
+# We now have to use the -g option to print them.
+# We then check that the output contains at least one debug symbol when relevant.
+
 make all
-test "$(objdump --syms "${NAME}" | grep -v "file format" | grep -c debug)" -eq 0
+test "$(objdump --syms -g "${NAME}" | grep -v "file format" | grep -c '\.debug')" -eq 0
 make debug
-test "$(objdump --syms "${NAME_DEBUG}" | grep -v "file format" | grep -c debug)" -gt 0
+test "$(objdump --syms -g "${NAME_DEBUG}" | grep -v "file format" | grep -c '\.debug')" -gt 0
 make fclean
 
 rm -f /tmp/release_output /tmp/debug_output
